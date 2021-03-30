10f71503348f79dd48c6e09f9b621985b9db58b4
Hollin <hollinisme@gmail.com>
2021-03-31 00:33:04 +0800

    clean code and extract classes

--- SDK_C++/CodeCraft-2021/CodeCraft-2021.cpp
+++ SDK_C++/CodeCraft-2021/CodeCraft-2021.cpp
@@ -13,10 +13,44 @@
 #include "definition/VirtualMachineModel.h"
 #include "definition/VirtualMachine.h"
 #include "definition/Server.h"
-#include "definition/CONSTANTS.h"
 #include "definition/OP.h"
 
 using namespace std;
+
+#ifdef DEBUG
+int addcount = 0;
+int delcount = 0;
+#endif
+
+// 用来生成服务器编号
+int localServerNum = 0;
+int globalServerNumber = 0;
+
+int MAX_VM_CORE = 0;
+int MAX_VM_MEMORY = 0;
+int MAX_SERVERMODEL_CORE = 0;
+int MAX_SERVERMODEL_MEMORY = 0;
+int MEAN_VM_CORE = 0;
+int MEAN_VM_MEMORY = 0;
+int VM_AMOUNT = 0;
+double MAX_SIMI = -1, MIN_SIMI = 0x3f3f3f3f;
+
+int MAXSOURCE = 0;
+
+//购买权重（和为1）
+double buy_PriceWeight = 0.4;//按价格购买权重
+double buy_BalanceWeight = 0.3;//两个节点使用资源比例平衡参数
+double buy_leftSpaceWeight = 0.0;//剩余空间
+double buy_PriceWithCapacituWeight = 0.3;//性价比
+double buy_SmWeight = 0.1;//相似性购买
+// double DayWeight = 0.8;
+
+//放置权值(和为1)
+double put_SelectWeight = 0.0;//碎片选择权重
+double put_NodeBlanceWeight = 0.4;//负载均衡参数
+double put_SimWeight = 0.;//相似放置
+double put_BalanceWeight = 0.4;//两个节点使用资源比例平衡参数
+double put_NearWeight = 0.0;//相近放置
 
 
 //***************************FUNCTION DEFINITION**********************************
@@ -686,7 +720,7 @@ void optimized_migrate(){
 #ifdef DEBUG
   assert(mVmidToVirtualMachine.size() == VM_AMOUNT);
 #endif
-    int totalOperation = 5 * VM_AMOUNT / 1000;
+    int totalOperation = 3 * VM_AMOUNT / 100;
     //int totalOperation = 3 * mVmidVirtualMachine.size() / 1000;
     // int totalOperation = 2 * mVmidVirtualMachine.size() / 1000;
 
@@ -776,7 +810,6 @@ void optimized_migrate(){
                     last -= 1;
                 }
             }
-            //break;
         }
         // 更新数据结构
         for(auto &p : modified){
@@ -851,7 +884,7 @@ int main()
     cin.tie(0);
     cout.tie(0);
 
-    int N, M, T;
+    int N, M, T, K;
     cin >> N;
     cin.ignore(); //忽略回车
     for(int i = 0; i < N; i++){
@@ -885,7 +918,7 @@ int main()
       }
     }
     //计算相似度
-    cin >> T;
+    cin >> T >> K;
 
     for(int i = 1; i <= T; i++){
         int R;
--- SDK_C++/CodeCraft-2021/definition/CONSTANTS.h
+++ SDK_C++/CodeCraft-2021/definition/CONSTANTS.h
@@ -1,6 +1,4 @@
-#pragma once
-
-// 操作类型
+//操作类型
 #define ADD 1
 #define DEL 2
 
@@ -9,40 +7,4 @@
 #define B 4
 #define BOTH 5
 #define NONE 0
-#define DEBUG
-
-#ifdef DEBUG
-extern int addcount;
-extern int delcount;
-#endif
-
-// 用来生成服务器编号
-extern int localServerNum;
-extern int globalServerNumber;
-
-extern int MAX_VM_CORE;
-extern int MAX_VM_MEMORY;
-extern int MAX_SERVERMODEL_CORE;
-extern int MAX_SERVERMODEL_MEMORY;
-extern int MEAN_VM_CORE;
-extern int MEAN_VM_MEMORY;
-extern int VM_AMOUNT;
-extern double MAX_SIMI;
-extern double MIN_SIMI;
-
-extern int MAXSOURCE;
-
-//购买权重（和为1）
-extern double buy_PriceWeight;//按价格购买权重
-extern double buy_BalanceWeight;//两个节点使用资源比例平衡参数
-extern double buy_leftSpaceWeight;//剩余空间
-extern double buy_PriceWithCapacituWeight;//性价比
-extern double buy_SmWeight;//相似性购买
-// double DayWeight = 0.8;
-
-//放置权值(和为1)
-extern double put_SelectWeight;//碎片选择权重
-extern double put_NodeBlanceWeight;//负载均衡参数
-extern double put_SimWeight;//相似放置
-extern double put_BalanceWeight;//两个节点使用资源比例平衡参数
-extern double put_NearWeight;//相近放置
+//#define DEBUG
